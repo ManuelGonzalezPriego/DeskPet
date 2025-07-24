797b2f1cb0793a59645d347dec35870999f077d4
Manuel <manuelgonzalezpriego1@gmail.com>
2025-07-24 18:52:58 +0200

    Pet moves and developing interction click

--- Assets/Scripts/Pet/Pet.cs
+++ Assets/Scripts/Pet/Pet.cs
@@ -1,7 +1,4 @@
-using System;
 using System.Collections;
-using System.Runtime.CompilerServices;
-using Unity.Mathematics;
 using UnityEngine;
 public class Pet : MonoBehaviour
 {
@@ -9,74 +6,72 @@ public class Pet : MonoBehaviour
     public int MAXLIFE = 100;
     public int MAXAGE = 18;
     public int MAXAPINES = 100;
-
-    public Transform WallL;
-    public Transform WallR;
-    private int direccion = 1;
-    private bool esperando = false;
-    private float velocidad = 2f;
+    private Vector2 direccion;
+    private bool stoped = false;
+    private float fast = 1f;
 
     private PetStats stats;
 
-    void Start()
+    private void Start()
     {
         // Initialize the pet stats with default values, this is a example, need to be changed
         stats = new PetStats(DEFAULTNAME, 0, MAXLIFE, MAXAPINES);
+
+        direccion = new Vector2(1, 0.3f).normalized;
     }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-
-        if (collision.transform == this.WallL || collision.transform == this.WallR)
-        {
-            Debug.Log("Pet collided with a wall: " + collision.gameObject.name);
-            // StartCoroutine((TurnAround()));
-        }
-
-        // if (collision.gameObject.CompareTag("Wall"))
-        // {
-        //     Debug.Log("Pet collided with a wall: " + collision.gameObject.name);
-        //     // If the pet collides with a wall, it will change direction
-        //     // This is a simple way to simulate a pet moving randomly and use coroutines to handle the waiting time.
-        //     if (esperando) return;
-
-        //     // Call the TurnAround coroutine to change direction
-        //     StartCoroutine(TurnAround());
-        // }
-        // else
-        // {
-        //     Console.WriteLine("Pet hit a wall, changing direction.");
-        //     // StartCoroutine((TurnAround()));
-        // }
-    }
-
-    // This function will change the direction of the pet when it hits a wall and use 
-    // IEnumerator to wait a random time before changing direction, this is a simple way to simulate a pet 
-    // moving randomly and use coroutines to handle the waiting time.
-    private IEnumerator TurnAround(){
-        esperando = true;
-
-        // Yield is used to pause the execution of the coroutine
-        yield return new WaitForSeconds(0.5f); // Stop for a random time
-        direccion *= -1;
-        Vector3 escala = transform.localScale;
-        escala.x *= -1;
-        transform.localScale = escala;
-        esperando = false;
-    }
-
 
     private void Update()
     {
         this.RandomMove();
     }
 
-    // In this fuction, the pet will move randomly between two walls
+    // Makes the pet move in a direction, unless it's in waiting state
     private void RandomMove()
     {
-        if (esperando) return;
+        if (stoped) return;
 
-        transform.Translate(Vector2.right * direccion * velocidad * Time.deltaTime);
+        transform.Translate(direccion * fast * Time.deltaTime);
+    }
+
+    // Check how to make the pet clickable
+    private void OnMouseDown()
+    {
+        Debug.Log("Pet clicked!");
+        StartCoroutine(StopBoing(1f));
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (stoped) return;
+
+
+        // Invert direction on X axis if hitting wall from left/right
+        if (Mathf.Abs(collision.contacts[0].normal.x) > 0.5f)
+        {
+            direccion.x *= -1;
+
+            // Flip the sprite horizontally to face the new direction
+            Vector3 escala = transform.localScale;
+            escala.x = Mathf.Sign(direccion.x) * Mathf.Abs(escala.x);
+            transform.localScale = escala;
+        }
+
+        // Invert direction on Y axis if hitting floor or ceiling
+        if (Mathf.Abs(collision.contacts[0].normal.y) > 0.5f)
+        {
+            direccion.y *= -1;
+        }
+
+        // Short delay to prevent rapid flipping
+        StartCoroutine(StopBoing(0.05f));
+    }
+
+    // Waits briefly after a bounce to prevent double collision processing
+    private IEnumerator StopBoing(float secons)
+    {
+        stoped = true;
+        yield return new WaitForSeconds(secons); 
+        stoped = false;
     }
 }
  
